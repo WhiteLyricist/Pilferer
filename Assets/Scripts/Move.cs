729d432ac1fed621aab46ba5d6a82d926d80b329
WhiteLyricist <57936182+WhiteLyricist@users.noreply.github.com>
2021-05-14 16:12:43 +0300

    v 0.1

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Move : MonoBehaviour
+{
+
+    public static Action<bool> OnStop = delegate { };
+
+    private float speed = 5f;
+    private bool update=false;
+    private Vector3 touch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneController.OnTouch += Movement;
+    }
+
+    void Movement(bool flag, Vector3 tab) 
+    {
+        update = flag;
+        touch = tab;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        OnStop(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!update) return;
+        Vector3 point =  Camera.main.ScreenToWorldPoint(touch);
+        point.z = transform.position.z;
+        transform.position = Vector3.MoveTowards(transform.position, point, speed * Time.deltaTime);
+        if (transform.position.x == point.x && transform.position.y == point.y) OnStop(false);
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SceneController : MonoBehaviour
+{
+
+    public static Action<bool, Vector3> OnTouch = delegate { };
+
+    // [SerializeField] private GameObject prefabDisk;
+
+    // private GameObject _disk;
+
+    //  public static Vector3 touch;
+     public static bool update = false;
+
+     private void Awake()
+      {
+        /* if (_disk == null)
+         {
+             _disk = Instantiate(prefabDisk) as GameObject;
+         }
+        */
+        Move.OnStop += Stop;
+      }
+
+    void Stop(bool flag)
+    {
+        update = flag;
+        OnTouch(update, Vector3.zero);
+    }
+
+    private void Update()
+    {
+        if  (Input.touchCount>0) 
+        {
+            Touch tab = Input.GetTouch(0);
+            if (tab.phase == TouchPhase.Began)
+            {
+                update = true;
+                OnTouch(update, tab.position);
+            }
+        }
+    }
+}
